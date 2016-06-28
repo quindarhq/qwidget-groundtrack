c78ecd80e3a23dc02479dcc60e85b29dfd1c111a
Ray Lai <rayymlai@gmail.com>
2016-06-28 13:10:06 -0700

    restructure example folder

---
+++ example/buildme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+// Program: buildme.sh
+// Purpose: to install NodeJS depencencies
+// Author:  Masaki Kakoi
+// Updated: Jun 28, 2016
+//
+echo "Building NodeJS dependencies for Quindar ground track"
+echo "...installing npm modules from package.json"
+npm install
+echo "...complete"
+
