56510359b2eeb8d5185bf1daeedb43b9a9ef1793
Ray Lai <rayymlai@gmail.com>
2016-07-14 17:01:33 -0700

    update client side package

--- example/config/clientSettings.js
+++ example/config/clientSettings.js
@@ -1,3 +1,9 @@
+// Program: clientSettings.js
+// Purpose: server end-point config
+// Author:  Masaki Kakoi
+// Updated: Jul 14, 2016
+// License: MIT License
+//
 var platform = 'platform.audacy.space';
 var dbPort = 7902;
 var port = 7904;
